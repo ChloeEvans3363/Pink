603738439164c3233682d89337029a51cc10185f
Chloe <cre3363@g.rit.edu>
2024-02-08 14:52:01 -0500

    Player can move with both keyboard and controller

--- Pink/Assets/Scripts/Player.cs
+++ Pink/Assets/Scripts/Player.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
 
+    private PlayerControls playerControls;
+
+    private Vector2 movementInput;
+
     public float speed = 12f;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +37,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * movementInput.x + transform.forward * movementInput.y;
 
         controller.Move(move * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        movementInput = ctx.ReadValue<Vector2>();
     }
 }
