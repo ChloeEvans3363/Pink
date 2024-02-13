3afca93f552bfff3802027c9c4842eb49470ab2a
Chloe <cre3363@g.rit.edu>
2024-02-12 20:53:12 -0500

    Added in camera movement on controller

--- Pink/Assets/Scripts/Player.cs
+++ Pink/Assets/Scripts/Player.cs
@@ -5,13 +5,17 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    // Platyer Movement
     private CharacterController controller;
-
     private PlayerControls playerControls;
-
     private Vector2 movementInput;
 
+    // Camera Movement
+    public Camera camera;
+    public float sensitivity = 100f;
+    float xRotation = 0f;
     public float speed = 12f;
+    private Vector2 cameraInput;
 
     private void Awake()
     {
@@ -32,6 +36,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -41,10 +47,25 @@ public class Player : MonoBehaviour
         Vector3 move = transform.right * movementInput.x + transform.forward * movementInput.y;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        // Camera
+        float cameraX = cameraInput.x * sensitivity * Time.deltaTime;
+        float cameray = cameraInput.y * sensitivity * Time.deltaTime;
+
+        xRotation -= cameray;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * cameraX);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnCameraMove(InputAction.CallbackContext ctx)
+    {
+        cameraInput = ctx.ReadValue<Vector2>();
     }
 }
