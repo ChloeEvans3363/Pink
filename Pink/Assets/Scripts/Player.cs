4420e6ffaddfde2744dab3162f051f922f3217a6
Chloe <cre3363@g.rit.edu>
2024-02-12 23:39:58 -0500

    Added gravity and jumping

--- Pink/Assets/Scripts/Player.cs
+++ Pink/Assets/Scripts/Player.cs
@@ -9,12 +9,21 @@ public class Player : MonoBehaviour
     private CharacterController controller;
     private PlayerControls playerControls;
     private Vector2 movementInput;
+    [SerializeField] private float speed = 12f;
+
+    // Gravity and Jumping
+    private Vector3 velocity;
+    [SerializeField] private float gravity = -19.62f;
+    [SerializeField] private float groundRaidus = 0.24f;
+    [SerializeField] private LayerMask groundLayers;
+    private float groundedOffset = 1.8f;
+    private bool grounded;
+    private float jumpHeight = 1.5f;
 
     // Camera Movement
-    public Camera camera;
-    public float sensitivity = 30f;
+    [SerializeField] private Camera camera;
+    [SerializeField] private float sensitivity = 30f;
     float xRotation = 0f;
-    public float speed = 12f;
     private Vector2 cameraInput;
 
     private void Awake()
@@ -43,6 +52,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset,
+            transform.position.z);
+
+        grounded = Physics.CheckSphere(spherePosition, groundRaidus, groundLayers);
+
+        if (grounded && velocity.y < 0)
+            velocity.y = -2f;
 
         // Move
         Vector3 move = transform.right * movementInput.x + transform.forward * movementInput.y;
@@ -58,6 +74,14 @@ public class Player : MonoBehaviour
 
         camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * cameraX);
+
+        if (playerControls.Gameplay.Jump.triggered && grounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        // Gravity and Jumping
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
