040cfe57eed066e1c89d8136128d0602aa14b130
Brian Egan <bpe4955@rit.edu>
2024-02-18 19:14:11 -0500

    respawning should be implemented

--- Pink/Assets/Scripts/Player.cs
+++ Pink/Assets/Scripts/Player.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 
 public class Player : MonoBehaviour
 {
     // Player Movement
     private CharacterController controller;
     private PlayerControls playerControls;
-    private Vector2 movementInput;
+    private UnityEngine.Vector2 movementInput;
     [SerializeField] private float speed = 12f;
 
     // Gravity and Jumping
-    private Vector3 velocity;
+    private UnityEngine.Vector3 velocity;
     [SerializeField] private float gravity = -19.62f;
     [SerializeField] private float groundRaidus = 0.24f;
     [SerializeField] private LayerMask groundLayers;
@@ -25,16 +27,17 @@ public class Player : MonoBehaviour
     [SerializeField] private Camera camera;
     [SerializeField] private float sensitivity = 30f;
     float xRotation = 0f;
-    private Vector2 cameraInput;
+    private UnityEngine.Vector2 cameraInput;
 
     //Combat
     [SerializeField] public GameObject bullet;
-    [SerializeField] public int health = 2;
+    private int health = 2;
     [SerializeField] private float invincibilityDuration = 1.0f;
     private float invincibilityTimer = 0;
     [SerializeField] private float reloadDuration = 2.0f;
     private float reloadTimer = 0;
     private bool shoot = false;
+    private GameObject[] spawnPoints;
 
     private void Awake()
     {
@@ -57,12 +60,14 @@ public class Player : MonoBehaviour
         controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset,
+        UnityEngine.Vector3 spherePosition = new UnityEngine.Vector3(transform.position.x, transform.position.y - groundedOffset,
             transform.position.z);
 
         grounded = Physics.CheckSphere(spherePosition, groundRaidus, groundLayers);
@@ -71,7 +76,7 @@ public class Player : MonoBehaviour
             velocity.y = -2f;
 
         // Move
-        Vector3 move = transform.right * movementInput.x + transform.forward * movementInput.y;
+        UnityEngine.Vector3 move = transform.right * movementInput.x + transform.forward * movementInput.y;
 
         controller.Move(move * speed * Time.deltaTime);
 
@@ -82,8 +87,8 @@ public class Player : MonoBehaviour
         xRotation -= cameraY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * cameraX);
+        camera.transform.localRotation = UnityEngine.Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(UnityEngine.Vector3.up * cameraX);
 
         if (jumped && grounded)
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -96,7 +101,7 @@ public class Player : MonoBehaviour
         // Combat
         if (reloadTimer <= 0 && shoot)
         {
-            GameObject bulletObj = Instantiate(bullet, this.transform.position + camera.transform.forward * 2.0f, Quaternion.identity);
+            GameObject bulletObj = Instantiate(bullet, this.transform.position + camera.transform.forward * 2.0f, UnityEngine.Quaternion.identity);
             bulletObj.GetComponent<Bullet>().Shoot(camera.transform.forward);
             reloadTimer = reloadDuration;
         }
@@ -106,12 +111,12 @@ public class Player : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        movementInput = ctx.ReadValue<Vector2>();
+        movementInput = ctx.ReadValue<UnityEngine.Vector2>();
     }
 
     public void OnCameraMove(InputAction.CallbackContext ctx)
     {
-        cameraInput = ctx.ReadValue<Vector2>();
+        cameraInput = ctx.ReadValue<UnityEngine.Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
@@ -124,17 +129,26 @@ public class Player : MonoBehaviour
         shoot = ctx.action.triggered;
     }
 
-    public void TakeDamage(int damage)
+    public bool TakeDamage(int damage)
     {
-        if(invincibilityTimer > 0) { return; }
-        Debug.Log("Took " + damage + " damage");
-        health -= damage;
+        if(invincibilityTimer > 0) { return false; }
+        this.health -= damage;
+        Debug.Log("Took " + damage + " damage. Has " + this.health + " health left.");
         invincibilityTimer = invincibilityDuration;
-        if(health <= 0) { Die(); }
+        if(this.health <= 0) {
+            Respawn();
+            return true;
+        }
+        return false;
     }
 
-    private void Die()
+    private void Respawn()
     {
-
+        gameObject.SetActive(false);
+        this.health = 2;
+        UnityEngine.Vector3 newPos = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+        Debug.Log("Player has died, respawning at "+newPos);
+        transform.position = newPos;
+        gameObject.SetActive(true);
     }
 }
