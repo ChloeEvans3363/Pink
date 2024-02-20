6c7885298fa174c519800cc3ead798e3e1bd7263
Chloe <cre3363@g.rit.edu>
2024-02-20 03:31:48 -0500

    Completely revamped movement

--- Pink/Assets/Scripts/Player.cs
+++ Pink/Assets/Scripts/Player.cs
@@ -12,19 +12,24 @@ public class Player : MonoBehaviour
     private CharacterController controller;
     private PlayerControls playerControls;
     private UnityEngine.Vector2 movementInput;
-    [SerializeField] private float speed = 12f;
-    private float accelerate = 6f;
-    private float drop = 5f;
+    private float accelerate = 500.0f;
+    private float friction = 15;
+    private float minFriction = 0.5f;
+    private float maxSpeed = 8f;
+    private UnityEngine.Vector3 velocity;
 
     // Gravity and Jumping
-    private UnityEngine.Vector3 velocity;
-    [SerializeField] private float gravity = -19.62f;
     [SerializeField] private float groundRaidus = 0.24f;
     [SerializeField] private LayerMask groundLayers;
     private float groundedOffset = 1.8f;
     private bool grounded;
-    private float jumpHeight = 1.5f;
     private bool jumped = false;
+    private float airControl = 16f;
+    private float airForwardAcceleration = 8f;
+    private float airAccelCoeff = 1f;
+    private float airDecelCoeff = 1.5f;
+    private float gravity = 24f;
+    private float jump = 8f;
 
     // Camera Movement
     [SerializeField] private Camera camera;
@@ -70,6 +75,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float time = Time.deltaTime;
         UnityEngine.Vector3 spherePosition = new UnityEngine.Vector3(transform.position.x, transform.position.y - groundedOffset,
             transform.position.z);
 
@@ -79,9 +85,8 @@ public class Player : MonoBehaviour
             velocity.y = -2f;
 
         // Wishdir
-        UnityEngine.Vector3 move = (transform.right * movementInput.x + transform.forward * movementInput.y).normalized;
-
-        //controller.Move(move * speed * Time.deltaTime);
+        UnityEngine.Vector3 move = new UnityEngine.Vector3(movementInput.x, 0, movementInput.y).normalized;
+        UnityEngine.Vector3 wishdir = UnityEngine.Vector3.ProjectOnPlane(transform.TransformDirection(move), UnityEngine.Vector3.down).normalized;
 
         // Camera
         float cameraX = cameraInput.x * sensitivity * Time.deltaTime;
@@ -93,16 +98,6 @@ public class Player : MonoBehaviour
         camera.transform.localRotation = UnityEngine.Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(UnityEngine.Vector3.up * cameraX);
 
-        if (jumped && grounded)
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-        if (grounded)
-            drop = 5f;
-        else
-            drop = 0;
-
-        // Gravity and Jumping
-        velocity.y += gravity * Time.deltaTime;
         
         // Combat
         if (reloadTimer <= 0 && shoot)
@@ -115,10 +110,32 @@ public class Player : MonoBehaviour
         reloadTimer -= Time.deltaTime;
 
         // Movement
-        applyAcceleration(move, speed, accelerate);
-        velocity = applyFriction(velocity, drop);
+        if (grounded)
+        {
+            ApplyFriction(time);
+            ApplyAcceleration(wishdir, accelerate, time);
+
+            velocity = UnityEngine.Vector3.ProjectOnPlane(velocity, UnityEngine.Vector3.down);
+            if (jumped)
+                velocity += -UnityEngine.Vector3.down * jump;
+        }
+        else
+        {
+            float coeff = UnityEngine.Vector3.Dot(velocity, wishdir) > 0 ? airAccelCoeff : airDecelCoeff;
+
+            ApplyAcceleration(wishdir, coeff, time);
+
+            if (Mathf.Abs(move.z) > 0.0001)
+            {
+                ApplyAirControl(move, wishdir, time);
+            }
+
+
+            velocity += UnityEngine.Vector3.down * (gravity * time);
+        }
+        
+
         controller.Move(velocity * Time.deltaTime);
-        Debug.Log(velocity);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -164,8 +181,8 @@ public class Player : MonoBehaviour
         gameObject.SetActive(true);
     }
 
-    // Credit to PhobicGunner for this code
-    private void applyAcceleration(UnityEngine.Vector3 wishDir, float wishSpeed, float acceleration)
+    // Credit to PhobicGunner and Atil for this code
+    private void ApplyAcceleration(UnityEngine.Vector3 wishDir, float acceleration, float time)
     {
         float y = velocity.y;
 
@@ -176,12 +193,12 @@ public class Player : MonoBehaviour
 
         currentSpeed = UnityEngine.Vector3.Dot(flatVel, wishDir);
 
-        addSpeed = wishSpeed - currentSpeed;
+        addSpeed = maxSpeed - currentSpeed;
 
         if (addSpeed <= 0f)
             return;
 
-        accelSpeed = acceleration * wishSpeed;
+        accelSpeed = acceleration * maxSpeed * time;
 
         if (accelSpeed > addSpeed)
             accelSpeed = addSpeed;
@@ -191,29 +208,64 @@ public class Player : MonoBehaviour
         velocity.y = y;
     }
 
-    // Credit to PhobicGunner for this code
-    private UnityEngine.Vector3 applyFriction(UnityEngine.Vector3 vel, float drop)
+    // Credit to PhobicGunner and Atil for this code
+
+    private void ApplyFriction(float time)
     {
-        float y = vel.y;
+        float y = velocity.y;
 
-        vel.y = 0f;
+        velocity.y = 0f;
 
-        float currentSpeed = vel.magnitude;
+        float currentSpeed = velocity.magnitude;
+        if (currentSpeed <= 0.00001f)
+        {
+            return;
+        }
 
-        float newSpeed = currentSpeed - drop;
+        float downLimit = Mathf.Max(currentSpeed, minFriction);
+        float dropAmount = currentSpeed - (downLimit * friction * time);
+        if (dropAmount < 0)
+        {
+            dropAmount = 0;
+        }
 
-        if(newSpeed < 0f)
-            newSpeed = 0f;
+        dropAmount /= currentSpeed;
 
-        newSpeed /= currentSpeed;
+        if (float.IsNaN(dropAmount))
+            velocity = UnityEngine.Vector3.up * y;
+        else
+        {
+            velocity *= dropAmount;
 
-        if (float.IsNaN(newSpeed))
-            return UnityEngine.Vector3.up * y;
+            velocity.y = y;
+        }
+    }
 
-        UnityEngine.Vector3 ret = vel * newSpeed;
+    // Credit to Atil for this code
+    private void ApplyAirControl(UnityEngine.Vector3 input, UnityEngine.Vector3 accelDir, float time)
+    {
+        UnityEngine.Vector3 playerHorizontalDirection = UnityEngine.Vector3.ProjectOnPlane(velocity, UnityEngine.Vector3.down).normalized;
+        float playerHorizontalSpeed = UnityEngine.Vector3.ProjectOnPlane(velocity, UnityEngine.Vector3.down).magnitude;
 
-        ret.y = y;
+        float dot = UnityEngine.Vector3.Dot(playerHorizontalDirection, accelDir);
+        if (dot > 0)
+        {
+            float k = airControl * dot * dot * time;
 
-        return ret;
+            bool isPureForward = Mathf.Abs(input.x) < 0.0001f && Mathf.Abs(input.z) > 0;
+            if (isPureForward)
+            {
+                k *= airForwardAcceleration;
+            }
+
+            // A little bit closer to accelDir
+            playerHorizontalDirection = playerHorizontalDirection * playerHorizontalSpeed + accelDir * k;
+            playerHorizontalDirection.Normalize();
+
+            // Assign new direction, without touching the vertical speed
+            velocity = UnityEngine.Vector3.ProjectOnPlane(playerHorizontalDirection * playerHorizontalSpeed, UnityEngine.Vector3.down) 
+                + UnityEngine.Vector3.up * (UnityEngine.Vector3.Dot(velocity, UnityEngine.Vector3.up));
+        }
+
     }
 }
