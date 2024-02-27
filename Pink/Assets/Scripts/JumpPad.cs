118486ac8884eace8cd82b3e024e8fca6482eac4
Chloe <cre3363@g.rit.edu>
2024-02-27 00:19:04 -0500

    Jumpad edits

--- Pink/Assets/Scripts/JumpPad.cs
+++ Pink/Assets/Scripts/JumpPad.cs
@@ -11,8 +11,7 @@ public class JumpPad : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player") { return; }
-        Debug.Log(other.gameObject.GetComponent<Player>().Jumped);
-        other.gameObject.GetComponent<Player>().Grounded = false;
+        other.gameObject.GetComponent<Player>().Grounded = true;
         playerJump = other.gameObject.GetComponent<Player>().Jump;
         other.gameObject.GetComponent<Player>().Jump = jump;
 
